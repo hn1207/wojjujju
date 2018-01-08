1f995cabce6cb1df963fcea706e1593c2f80f0c8
jinmo <jinmo@softsquared.com>
2018-01-08 15:48:07 +0900

    beauty page update

--- app/src/main/java/com/ylabs/wojjujju/wojjujju/activity/BeautyActivity.java
+++ app/src/main/java/com/ylabs/wojjujju/wojjujju/activity/BeautyActivity.java
@@ -1,10 +1,6 @@
 package com.ylabs.wojjujju.wojjujju.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -12,15 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.ylabs.wojjujju.wojjujju.R;
 import com.ylabs.wojjujju.wojjujju.adapter.BeautyDefaultItemAdapter;
-import com.ylabs.wojjujju.wojjujju.adapter.HomeHotPlaceItemAdapter;
 import com.ylabs.wojjujju.wojjujju.entity.BeautyItem;
 
 import java.util.ArrayList;
@@ -45,9 +38,6 @@ public class BeautyActivity extends AppCompatActivity{
         setSupportActionBar(toolbar);
 
 
-
-
-
         initSpinner();
         initBeautyDefaultItemList();
 
@@ -59,13 +49,13 @@ public class BeautyActivity extends AppCompatActivity{
         // Spinner
         Spinner defaultSpinner = (Spinner)findViewById(R.id.default_filter);
         ArrayAdapter yearAdapter = ArrayAdapter.createFromResource(this,
-                R.array.default_filter, android.R.layout.simple_spinner_item);
+                R.array.default_filter, R.layout.spinner);
         yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         defaultSpinner.setAdapter(yearAdapter);
 
         Spinner placeSpinner = (Spinner)findViewById(R.id.place_filter);
         ArrayAdapter monthAdapter = ArrayAdapter.createFromResource(this,
-                R.array.place_filter, android.R.layout.simple_spinner_item);
+                R.array.place_filter, R.layout.spinner);
         monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         placeSpinner.setAdapter(monthAdapter);
 
@@ -76,20 +66,20 @@ public class BeautyActivity extends AppCompatActivity{
 
         ArrayList<BeautyItem> beautyItemArrayList = new ArrayList<BeautyItem>();
 
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 2, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
-        beautyItemArrayList.add(new BeautyItem(null, "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 0));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
+        beautyItemArrayList.add(new BeautyItem("http://13.125.46.183/woojjujju/beauty.jpeg", "댕댕이 미용", "0", "서울시 서초구 서초동", 4, 785, "예약취소 연기 가능","예약특가", 1));
 
 
 
