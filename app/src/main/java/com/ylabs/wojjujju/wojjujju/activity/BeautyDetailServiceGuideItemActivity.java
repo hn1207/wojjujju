c64e9b86846216036a5a423827fa5e86c02fc1d3
Hanul Lee <hanul@softsquared.com>
2018-01-09 14:40:43 +0900

    Beauty Complete

---
+++ app/src/main/java/com/ylabs/wojjujju/wojjujju/activity/BeautyDetailServiceGuideItemActivity.java
@@ -0,0 +1,30 @@
+package com.ylabs.wojjujju.wojjujju.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.ylabs.wojjujju.wojjujju.R;
+import com.ylabs.wojjujju.wojjujju.adapter.BaseItemAdapter;
+import com.ylabs.wojjujju.wojjujju.custom.RecyclerItemClickListener;
+import com.ylabs.wojjujju.wojjujju.entity.Item;
+
+import java.util.ArrayList;
+
+public class BeautyDetailServiceGuideItemActivity extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beauty_detail_service_guide_item);
+
+
+    }
+
+
+}
